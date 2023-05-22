b262015086208e6e2a09278dba6bb0d32f43c821
Adam K <50672995+kardell2006g@users.noreply.github.com>
2023-05-22 10:40:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     tfe = {
       source = "hashicorp/tfe"
-      version = "~> 0.25.3"
+      version = "~> 0.44.1"
     }
     null = {
       source = "hashicorp/null"
@@ -51,14 +51,13 @@ resource "tfe_team_access" "test" {
   team_id = tfe_team.test.id
   workspace_id = each.value
 }
-  
-  
+   
   resource "tfe_project" "test" {
   name         = var.tf_project
   organization = var.tf_organization
 }
 
-resource "tfe_team_project_access" "admin" {
+resource "tfe_team_project_access" "test" {
   access       = "admin"
   team_id      = tfe_team.test.id
   project_id   = tfe_project.test.id
